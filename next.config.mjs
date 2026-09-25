3a02788475e3aa8ac7337dcfc7f757bf21f3676a
Nour Elyakine Ben Aicha <141434046+nourbenaicha90@users.noreply.github.com>
2026-09-25 21:18:01 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,7 @@ const nextConfig = {
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "res.cloudinary.com" },
+      { protocol: "https", hostname: "*.fbcdn.net" },
     ],
   },
 };
